6d683e9c8efd96bd21598dde4107800a4f054bf2
Divya Chhabra <divichhabra49@gmail.com>
2025-04-08 20:07:08 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
-//import { Navigate, useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const Header = () => {
 
   const {userData}=useContext(AppContext)
-  //const navigate=useNavigate()
+  const navigate=useNavigate()
   //console.log("userData in Header:", userData);
 
 
@@ -17,7 +17,9 @@ const Header = () => {
             <img src={assets.hand_wave} className='w-8 aspect-square'/></h1>
         <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our app</h2>
         <p className='mb-8 max-w-md'>Lets get Started</p>
-        <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
+        <button 
+        onClick={()=>navigate("/login")}
+        className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 
 const Home = () => {
-  useEffect(() => {
-    toast.info('Please allow 3rd party cookies for this project.', {
-      position: 'top-left',
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      style: {
-        background: '#ffffff',           // White background
-        color: '#000000',                // Black text
-        borderLeft: '6px solid #22c55e', // Green accent (Tailwind green-500)
-        boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
-        borderRadius: '10px',
-        fontWeight: '500',
-      },
-      progressStyle: {
-        color: '#22c55e',           // Green progress bar
-      },
-    });
-  }, []);
+  
+    toast.success('Please allow 3rd party cookies for this project.');
 
   return (
     <div className='flex flex-col items-center min-h-screen bg-[url("/bd_img.png")] bg-cover bg-center'>
